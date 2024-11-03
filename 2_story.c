342ff450d1f4fa15521e66f936db1b12ef8f0eab
Liam Shanahan <lpshano@gmail.com>
2024-11-03 07:22:41 +0100

    variable overview in C

--- 2_story.c
+++ 2_story.c
@@ -3,5 +3,9 @@
 
  int main() {
     char characterName[] = "John";
+    int characterAge = 35;
     printf("there once was a man named %s \n", characterName);
+    printf("%s was %d years old\n", characterName, characterAge);
+
+    return 0;
  }
