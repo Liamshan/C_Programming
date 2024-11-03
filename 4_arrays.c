7755c8c6f157afa968b50129f289ccd717e38cd5
Liam Shanahan <lpshano@gmail.com>
2024-11-03 08:22:30 +0100

    arrays added

---
+++ 4_arrays.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+
+int main() {
+    int luckyNumbers[] = {4, 8, 15, 16, 23}; 
+    int luckyBoys[20];  //If you don't assign values up front, then you need to declare the size. 
+
+    printf("%d", luckyNumbers[0]);   //print index position 0
+
+    return 0;
+}
