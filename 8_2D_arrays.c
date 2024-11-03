dc1a553fb03a81a49f492d36c758de6e1609f504
Liam Shanahan <lpshano@gmail.com>
2024-11-03 09:30:37 +0100

    2D arrays and nested for loops with them

---
+++ 8_2D_arrays.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+
+ int main() {
+    int nums[3][2] = {
+                    {1, 2},
+                    {3, 4},
+                    {5, 6}
+                    };
+    //printf("%d", nums[1][1]);  //Print an element of the array
+
+    // Nested for loop to parse thru the 2D array
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ",nums[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+ }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C Programming
 Here i am learning the basics of C.
+
+Course followed is here:
+https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m&ab_channel=freeCodeCamp.org
